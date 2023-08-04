c66f35da2dffd5008a98491a8fc7c63b4dafc83c
Vural Sayan <vuralsayan01@gmail.com>
2023-08-04 17:57:40 +0300

    Satış işlemleri tamamlandı

--- SatisProje/Form1.Designer.cs
+++ SatisProje/Form1.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CmbUrun = new System.Windows.Forms.ComboBox();
+            this.CmbMusteri = new System.Windows.Forms.ComboBox();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.CmbPersonel = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtFiyat = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,11 +52,97 @@
             this.dataGridView2.Size = new System.Drawing.Size(1202, 241);
             this.dataGridView2.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürün:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Müşteri:";
+            // 
+            // CmbUrun
+            // 
+            this.CmbUrun.FormattingEnabled = true;
+            this.CmbUrun.Location = new System.Drawing.Point(124, 283);
+            this.CmbUrun.Name = "CmbUrun";
+            this.CmbUrun.Size = new System.Drawing.Size(189, 32);
+            this.CmbUrun.TabIndex = 4;
+            // 
+            // CmbMusteri
+            // 
+            this.CmbMusteri.FormattingEnabled = true;
+            this.CmbMusteri.Location = new System.Drawing.Point(124, 352);
+            this.CmbMusteri.Name = "CmbMusteri";
+            this.CmbMusteri.Size = new System.Drawing.Size(189, 32);
+            this.CmbMusteri.TabIndex = 5;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(373, 354);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(130, 36);
+            this.BtnKaydet.TabIndex = 8;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // CmbPersonel
+            // 
+            this.CmbPersonel.FormattingEnabled = true;
+            this.CmbPersonel.Location = new System.Drawing.Point(124, 421);
+            this.CmbPersonel.Name = "CmbPersonel";
+            this.CmbPersonel.Size = new System.Drawing.Size(189, 32);
+            this.CmbPersonel.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 498);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Fiyat:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Personel:";
+            // 
+            // TxtFiyat
+            // 
+            this.TxtFiyat.Location = new System.Drawing.Point(124, 492);
+            this.TxtFiyat.Name = "TxtFiyat";
+            this.TxtFiyat.Size = new System.Drawing.Size(189, 30);
+            this.TxtFiyat.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 608);
+            this.Controls.Add(this.TxtFiyat);
+            this.Controls.Add(this.CmbPersonel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.CmbMusteri);
+            this.Controls.Add(this.CmbUrun);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
             this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -56,11 +151,21 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CmbUrun;
+        private System.Windows.Forms.ComboBox CmbMusteri;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.ComboBox CmbPersonel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtFiyat;
     }
 }
 
--- SatisProje/Form1.cs
+++ SatisProje/Form1.cs
@@ -20,12 +20,65 @@ namespace SatisProje
 
         SqlConnection baglanti = new SqlConnection(@"Data Source=Vural\SQLEXPRESS;Initial Catalog=DbSatis;Integrated Security=True");
 
+
+        private void Yenile()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute Satis", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView2.DataSource = dt;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            SqlDataAdapter da = new SqlDataAdapter("Execute Satis",baglanti); // DataAdapter, veritabanı ile arayüz arasında köprü görevi görür.
-            DataTable dt = new DataTable();                // DataTable, veritabanından gelen verileri tutar.
-            da.Fill(dt);                                    // Fill metodu, veritabanından gelen verileri DataTable'a doldurur.
+            SqlDataAdapter da = new SqlDataAdapter("Execute Satis",baglanti);  // DataAdapter, veritabanı ile arayüz arasında köprü görevi görür.
+            DataTable dt = new DataTable();                                   // DataTable, veritabanından gelen verileri tutar.
+            da.Fill(dt);                                                     // Fill metodu, veritabanından gelen verileri DataTable'a doldurur.
             dataGridView2.DataSource = dt;
+
+            //Ürünleri Çekme
+            SqlCommand komut2 = new SqlCommand("Select * From URUNLER", baglanti);
+            SqlDataAdapter da2 = new SqlDataAdapter(komut2);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            CmbUrun.ValueMember = "ID";   
+            CmbUrun.DisplayMember = "AD";
+            CmbUrun.DataSource = dt2;
+
+            //Müşterileri Çekme
+            SqlCommand komut3 = new SqlCommand("Select * From MUSTERILER", baglanti);
+            SqlDataAdapter da3 = new SqlDataAdapter(komut3); 
+            DataTable dt3 = new DataTable();
+            da3.Fill(dt3);
+            CmbMusteri.ValueMember = "ID";
+            CmbMusteri.DisplayMember = "ADSOYAD";
+            CmbMusteri.DataSource = dt3;
+
+            //Personelleri Çekme    
+            SqlCommand komut4 = new SqlCommand("Select * From PERSONELLER", baglanti);
+            SqlDataAdapter da4 = new SqlDataAdapter(komut4);
+            DataTable dt4 = new DataTable();    
+            da4.Fill(dt4);
+            CmbPersonel.ValueMember = "ID";
+            CmbPersonel.DisplayMember = "AD";
+            CmbPersonel.DataSource = dt4;
+
+            
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO HAREKETLER (URUN, MUSTERI, PERSONEL, FIYAT) VALUES(@P1,@P2,@P3,@P4)",baglanti);
+            komut.Parameters.AddWithValue("@P1", CmbUrun.SelectedValue);
+            komut.Parameters.AddWithValue("@P2", CmbMusteri.SelectedValue);
+            komut.Parameters.AddWithValue("@P3", CmbPersonel.SelectedValue);
+            komut.Parameters.AddWithValue("@P4", TxtFiyat.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Satış Eklendi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            Yenile();
         }
     }
 }
+ 
