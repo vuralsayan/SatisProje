092a2d62df61fcdcbcea48dc8be0fbc2d7a93541
Vural Sayan <vuralsayan01@gmail.com>
2023-08-04 15:41:36 +0300

    Sihirbaz ve SQL kullanımı eklendi

--- SatisProje/Form1.cs
+++ SatisProje/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SatisProje
 {
@@ -15,6 +16,19 @@ namespace SatisProje
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=Vural\SQLEXPRESS;Initial Catalog=DbSatis;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbSatisDataSet.Satis' table. You can move, or remove it, as needed.
+            this.satisTableAdapter.Fill(this.dbSatisDataSet.Satis);
+
+            SqlDataAdapter da = new SqlDataAdapter("Execute Satis",baglanti); // DataAdapter, veritabanı ile arayüz arasında köprü görevi görür.
+            DataTable dt = new DataTable();                // DataTable, veritabanından gelen verileri tutar.
+            da.Fill(dt);                                    // Fill metodu, veritabanından gelen verileri DataTable'a doldurur.
+            dataGridView2.DataSource = dt;
         }
     }
 }
